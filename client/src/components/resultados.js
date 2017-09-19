74d38eca378f76745a3976385e12f40e5e16fc9a
Gabriel Sanchez <gabrielsanchez@Gabriels-MacBook-Pro.local>
2017-09-18 23:06:05 -0600

    arregla el problema del orden de los jugadores en la tabla general

--- client/src/components/resultados.js
+++ client/src/components/resultados.js
@@ -139,7 +139,7 @@ const ResultadosComponent = (props) => {
   let pointsStr = ''
   usersWithPoints.forEach(u => pointsStr += `username: ${u.username}, points: ${u.totalPoints} - `);
   alert(pointsStr);
-  let pointsStr = ''
+  pointsStr = ''
 
   usersWithPoints.sort((a, b) => a.totalPoints - b.totalPoints);
   usersWithPoints.forEach(u => pointsStr += `username: ${u.username}, points: ${u.totalPoints} - `);
